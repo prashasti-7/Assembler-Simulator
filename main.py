e23c963d819071541c779dd3f41669a715b773b2
prashasti-7 <prashasti21346@iiitd.ac.in>
2022-07-02 11:54:18 +0530

    infinite input

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-regcode = ['R0','R1','R2','R3','R4','R5','R6']
-address_reg = ['000','001','010','011','100','101','110','111']
-regcode_dict = dict(zip(regcode, address_reg))
-print(regcode_dict)
+opCodes={'add':'10000','sub':'10001','mov':['10010','10011'],'ld':'10100',
+        'st':'10101','mul':'10110','div':'10111','rs':'11000',
+        'ls':'11001','xor':'11010','or':'11011','and':'11100',
+        'not':'11101', 'cmp':'11110','jmp':'11111','jlt':'01100',
+        'jgt':'01101','je':'01111','hlt':'01010'}                      #'mov':10010--->immediate  'mov':10011--->register   
 
-opcode = ['10000','10001','10010','10011','10100','10101','10110','10111','11000','11001','11010','11011','11100','11101','11110','11111','01100','01101','01111','01010']
-ist = ['add','sub','mov','mov','ld','st','mul','div','rs','ls','xor','or','and','not','cmp','jmp','jlt','jgt','je','hlt']
-opcode_dict = dict(zip(opcode, ist))
-print(opcode_dict)
-
+Registers={'R0':'000','R1':'001','R2':'010','R3':'011',
+'R4':'100','R5':'101','R6':'110','FLAGS':'111'}    
 while(True):
-    inp = list(map(str,input().split())) 
-    if (inp[0]=='add'):
-        print("WOW")
+    inp=list(map(str,input().split()))
+    if(inp[0]=='add'):
+        print("wow")
