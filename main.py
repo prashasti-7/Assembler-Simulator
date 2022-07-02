012d60ec2a93fd3eb3c257cf5de73b2302eb78ed
prashasti-7 <prashasti21346@iiitd.ac.in>
2022-07-02 15:12:38 +0530

    alltypes

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ reg_code={'R0':'000','R1':'001','R2':'010','R3':'011',
 'R4':'100','R5':'101','R6':'110','FLAGS':'111'}    
 while(True):
     inp=list(map(str,input().split()))
-    # Type A:
 
+    # Type A:
     if(inp[0]=='add'):
         if(inp[1] in reg_code and inp[2] in reg_code and inp[3] in reg_code):
             print(op_code['add']+'00' + reg_code[inp[1]] + reg_code[inp[2]] + reg_code[inp[3]])
@@ -43,8 +43,8 @@ while(True):
     elif(inp[0]=='and'):
         if(inp[1] in reg_code and inp[2] in reg_code and inp[3] in reg_code):
             print(op_code['and']+'00' + reg_code[inp[1]] + reg_code[inp[2]] + reg_code[inp[3]])
-    # Type B:
 
+    # Type B:
     elif(inp[0]=='mov'):
         if(inp[1] in reg_code):
             word = inp[2]
@@ -59,7 +59,72 @@ while(True):
                 t = '0'*(8-length)
                 print(op_code['mov'][1] + reg_code[inp[1]] + t + value)
             else:
-                print(op_code['mov'][2] + '00000' + reg_code[inp[1]] + reg_code[inp[2]] )
+                if(inp[2] in reg_code):  #Type C
+                    print(op_code['mov'][2] + '00000' + reg_code[inp[1]] + reg_code[inp[2]] )
     
-    elif (inp[0]=='hlt'):
+    elif(inp[0]=='ls'):
+        if(inp[1] in reg_code):
+            word = inp[2]
+            word_list = list(word)
+            if(word_list[0]=='$'):
+                word_list.remove('$')
+                word_final = ''.join(word_list)
+                decimal_number = str(word_final)
+                value = dec_to_bi(decimal_number)
+                value  = str(value)
+                length = len(value)
+                t = '0'*(8-length)
+                print(op_code['ls'] + reg_code[inp[1]] + t + value)
+
+    elif(inp[0]=='rs'):
+        if(inp[1] in reg_code):
+            word = inp[2]
+            word_list = list(word)
+            if(word_list[0]=='$'):
+                word_list.remove('$')
+                word_final = ''.join(word_list)
+                decimal_number = str(word_final)
+                value = dec_to_bi(decimal_number)
+                value  = str(value)
+                length = len(value)
+                t = '0'*(8-length)
+                print(op_code['rs'] + reg_code[inp[1]] + t + value)                
+
+    # Type C:
+    elif(inp[0]=='div'):
+        if(inp[1] in reg_code and inp[2] in reg_code):
+            print(op_code['div']+'00000' + reg_code[inp[1]] + reg_code[inp[2]])            
+
+    elif(inp[0]=='not'):
+        if(inp[1] in reg_code and inp[2] in reg_code):
+            print(op_code['not']+'00000' + reg_code[inp[1]] + reg_code[inp[2]])         
+
+    elif(inp[0]=='cmp'):
+        if(inp[1] in reg_code and inp[2] in reg_code):
+            print(op_code['cmp']+'00000' + reg_code[inp[1]] + reg_code[inp[2]])         
+
+    #Type D
+    elif(inp[0]=='ld'):
+        if(inp[1] in reg_code):
+            print(op_code['ld'] + reg_code[inp[1]] + inp[2])     ######doubt
+
+    elif(inp[0]=='st'):
+        if(inp[1] in reg_code):
+            print(op_code['st'] + reg_code[inp[1]] + inp[2])     ######doubt
+
+    #Type E
+    elif(inp[0]=='jmp'):
+        print(op_code['jmp'] + inp[1])     ######doubt
+
+    elif(inp[0]=='jlt'):
+        print(op_code['jlt'] + inp[1])     ######doubt
+
+    elif(inp[0]=='jgt'):
+        print(op_code['jgt'] + inp[1])     ######doubt      
+
+    elif(inp[0]=='je'):
+        print(op_code['je'] + inp[1])     ######doubt   
+
+    #Type F
+    elif(inp[0]=='hlt'):
         break
