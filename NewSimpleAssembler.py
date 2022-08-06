33986a2bc36553d4d9c2bc853094a57b9b247af4
shruti-353 <108338065+shruti-353@users.noreply.github.com>
2022-08-06 16:17:07 +0530

    Update NewSimpleAssembler.py

--- NewSimpleAssembler.py
+++ NewSimpleAssembler.py
@@ -226,6 +226,10 @@ for i in range(len_list):
     elif(input_list[i][0]=='mov' and len_inst == 3):
         if(input_list[i][1] in reg_code):
             movInst()
+
+        elif(input_list[i][1] == 'FLAGS'):
+            final_print.append(op_code['mov'][2] + '00000' + '111' + reg_code[input_list[i][2]])
+            
         else:
             error_list.append("ERROR!Register format incorrect."+"-"+"Line "+str(i))
         i+=1
